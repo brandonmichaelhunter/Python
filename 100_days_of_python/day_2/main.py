8b2178c13274c3021d6a4b289bafd7ee477b8fab
Brandon M Hunter <brandonmichaelhunter@gmail.com>
2024-10-15 07:19:57 -0400

    Completed day 2

---
+++ 100_days_of_python/day_2/main.py
@@ -0,0 +1,16 @@
+# Day 2 Challenge
+"""
+Write code that does the following:
+1. Asks for the user's name, favorite food, favorite music and where they live (or you can create other questions!)
+2. Store all of the answers in different variables.
+3. Print out a full sentence that includes the user's favorite things.
+4. Give them a positive affirmation at the end - tell them they rock at doing something!
+"""
+userName = input("What is your name? ")
+favoriteFood = input("What is your favorite food? ")
+favoriteMusic = input("What is your favorite song? ")
+userHome = input("What city do you live in? ")
+print("Hi "+userName+", good morning. I hope you are having a great day. Here is what I know about you.")
+print("Your favorite food is "+ favoriteFood)
+print("Your favorite song is "+ favoriteMusic)
+print("and you live in the city of "+userHome+".")
